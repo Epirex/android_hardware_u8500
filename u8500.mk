609bcbf874273c749c7e8421aa0beb81611c9f90
Esteban Guzzo <estebanguzzo@gmail.com>
2017-09-17 12:15:25 -0300

    Display: Don't build gralloc anymore
    Why? check this:
    https://github.com/Epirex/android_vendor_samsung_u8500/commit/bb791e701b00a171153488a814a535a1841cd61e

--- u8500.mk
+++ u8500.mk
@@ -6,7 +6,6 @@ $(call inherit-product, hardware/u8500/audio/libasound/alsa-lib-products.mk)
 # STE Display 
 PRODUCT_PACKAGES += \
    lights.montblanc \
-   gralloc.montblanc \
    hwcomposer.montblanc \
    copybit.montblanc \
    libblt_hw
